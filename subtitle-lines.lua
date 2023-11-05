8368e8285870e223b5ae7fa97dac5d1237bef972
Lypheo <45497420+Lypheo@users.noreply.github.com>
2023-11-05 23:27:03 +0100

    Clean up

--- subtitle-lines.lua
+++ subtitle-lines.lua
@@ -29,13 +29,9 @@ function parseDialogue(input)
     end
 end
 
-
----@type {start:number;stop:number;line:string}[]|nil
 local subtitles = nil
 local menu_open = false
 
----Get lines form current subtitle track
----@return {start:number;stop:number;line:string}[]
 function acquire_subtitles()
     subtitles = {}
     local path = mp.get_property("path")
@@ -43,49 +39,27 @@ function acquire_subtitles()
 
     local cmd = "ffmpeg -i \"" .. path .. "\" -map s:"..sid .. " -f ass - 2> nul"
     local handle = io.popen(cmd, 'r')
-    print(cmd, handle)
     if not handle then
-        return nil
+        mp.msg.error("Couldn’t open sub demuxer with command: " ..  cmd)
+        return
     end
     local select_idx = 0
     for line in handle:lines() do
+        mp.dispatch_events() -- this allows the script to remain (somewhat) responsive during demuxing so that the menu can be closed and re-opened
+        if not subtitles then break end -- end-file received, abort
+        -- not sure if this is more fragile than calling mp_event_loop()?
         local parsed_line = parseDialogue(line)
         if parsed_line then
             table.insert(subtitles, parsed_line)
-            -- print(utils.to_string(parsed_line), menu_open)
             if menu_open then
-                local time = mp.get_property_number('time-pos')
-                if parsed_line.start <= time and time <= parsed_line.stop then
-                    select_idx = #subtitles
-                end
                 show_subtitle_list()
             end 
         end
     end
     handle:close()
-    -- show_subtitle_list(select_idx)
-    return true
 end
 
-function show_loading_indicator()
-    local menu = {
-        title = 'Subtitle lines',
-        items = { {
-            title = 'Loading...',
-            icon = 'spinner',
-            italic = true,
-            muted = true,
-            selectable = false,
-            value = 'ignore',
-        } },
-        type = 'subtitle-lines-loading',
-    }
-
-    local json = utils.format_json(menu)
-    mp.commandv('script-message-to', 'uosc', 'open-menu', json)
-end
-
-function show_subtitle_list(select_idx)
+function show_subtitle_list()
     local menu = {
         title = 'Subtitle lines',
         items = {},
@@ -96,17 +70,13 @@ function show_subtitle_list(select_idx)
             'uosc-menu-closed',
         }
     }
-    if select_idx then
-        menu.selected_index = select_idx
-    end
 
-    local time = mp.get_property_number('time-pos')
+    local time = mp.get_property_number('time-pos', 0)
     for _, subtitle in ipairs(subtitles or {}) do
         menu.items[#menu.items + 1] = {
             title = subtitle.line,
             hint = subtitle.start_s .. ' – ' .. subtitle.stop_s,
-            active = subtitle.start <= time and time <= subtitle.stop or _ == select_idx,
-            -- active = _ == select_idx,
+            active = subtitle.start <= time and time <= subtitle.stop,
             value = {
                 'seek',
                 subtitle.start,
@@ -121,13 +91,12 @@ function show_subtitle_list(select_idx)
     menu_open = true
 end
 
-mp.add_key_binding("F6", 'list_subtitles', function()
+mp.add_key_binding(nil, 'list_subtitles', function()
     if menu_open then
         mp.commandv('script-message-to', 'uosc', 'close-menu', 'subtitle-lines-list')
         return
     end
 
-    show_loading_indicator()
     show_subtitle_list()
     if not subtitles then
         acquire_subtitles()
